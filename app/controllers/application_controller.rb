941ac807d24a86829d569e04e73e2cb1799ac0ab
Mikel Lindsaar <mikel@reinteractive.net>
2012-11-21 15:33:55 +1100

    Make sure we actually call the redirect code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  before_filter :redirect_to_charges_page
+
   protect_from_forgery
+
+  private
 
   def redirect_to_charges_page
     if 'exec-io.com$' =~ request.host
