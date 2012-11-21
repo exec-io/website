a0b42cbe47d78e971dcbcd4bf76828d1884b79cd
Mikel Lindsaar <mikel@reinteractive.net>
2012-11-21 15:32:18 +1100

    Adding redirect for exec-io.com

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def redirect_to_charges_page
+    if 'exec-io.com$' =~ request.host
+      redirect_to 'http://exec.io/charges'
+      return false
+    end
+  end
+
 end
