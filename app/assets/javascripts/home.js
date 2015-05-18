d56f29d98dba3c84491f571803fed9f184c57cc7
Mikel Lindsaar <mikel@reinteractive.net>
2015-05-18 18:44:04 +1000

    Fix up link backgrounds

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -10,36 +10,32 @@ var resizeWindow = function() {
 $(document).on('scroll', function(){
   var panels = $('section.home-panel').length;
 
-  var heightP1 = $('section.home-panel:nth-child(10n+2)').outerHeight();  
+  var heightP1 = $('section.home-panel:nth-child(10n+2)').outerHeight();
   var heightP2 = $('section.home-panel:nth-child(10n+3)').outerHeight() + heightP1;
   var heightP3 = $('section.home-panel:nth-child(10n+4)').outerHeight() + heightP2;
   var heightP4 = $('section.home-panel:nth-child(10n+5)').outerHeight() + heightP3;
-  var heightP5 = $('section.home-panel:nth-child(10n+5)').outerHeight() + heightP4;
-  var heightP6 = $('section.home-panel:nth-child(10n+5)').outerHeight() + heightP5;
-  var scrollPos = $(window).scrollTop();  
-  if (scrollPos >= heightP6) {
+  var heightP5 = $('section.home-panel:nth-child(10n+6)').outerHeight() + heightP4;
+  var heightP6 = $('section.home-panel:nth-child(10n+7)').outerHeight() + heightP5;
+  var scrollPos = $(window).scrollTop();
+  if (scrollPos >= heightP5) {
     $('#links a').removeClass('active');
     $('#links a[data-link=locations]').addClass('active');
-    $('body').removeClass().addClass('s6');
-  } else if (scrollPos >= heightP5) {
-    $('#links a').removeClass('active');
-    $('#links a[data-link=our-board]').addClass('active');
     $('body').removeClass().addClass('s5');
   } else if (scrollPos >= heightP4) {
     $('#links a').removeClass('active');
-    $('#links a[data-link=about-us]').addClass('active');
+    $('#links a[data-link=our-board]').addClass('active');
     $('body').removeClass().addClass('s4');
   } else if (scrollPos >= heightP3) {
     $('#links a').removeClass('active');
-    $('#links a[data-link=about-rethink-training]').addClass('active');
+    $('#links a[data-link=about-us]').addClass('active');
     $('body').removeClass().addClass('s3');
   } else if (scrollPos >= heightP2) {
     $('#links a').removeClass('active');
-    $('#links a[data-link=about-real-statistics]').addClass('active');
+    $('#links a[data-link=about-pp]').addClass('active');
     $('body').removeClass().addClass('s2');
   } else if (scrollPos >= heightP1) {
     $('#links a').removeClass('active');
-    $('#links a[data-link=about-pp]').addClass('active');
+    $('#links a[data-link=about-envisage]').addClass('active');
     $('body').removeClass().addClass('s1');
   } else if (scrollPos < heightP1) {
     $('#links a').removeClass('active');
