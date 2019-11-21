0016d8dcaf581f9b5ff3154aabc87a4254815103
Mikel Lindsaar <mikel@reinteractive.net>
2019-11-21 12:27:46 -0800

    Remove root route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ ExecIo::Application.routes.draw do
   ActiveAdmin.routes(self)
 
   scope "(:locale)", :locale => /en|de|es|fr|it|hu|ja|ru|zh-TW|he|sk|sv/ do
-    root :to => 'welcome#show'
+    #root :to => 'welcome#show'
     get '/blog' => 'blog#index', :as => :blog
   end
 
